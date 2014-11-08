7eb055ce958fa43661219e47bd0970f88030a7a8
Vadimyan <volleyballist@volleyballist.ru>
2014-11-09 00:21:00 +0300

    Delete obsolete files

--- ConsoleApplication1/ILogRecordFormatter.cs
+++
@@ -1,9 +0,0 @@
-using Rikrop.Core.Framework.Logging;
-
-namespace ConsoleApplication1
-{
-    public interface ILogRecordFormatter
-    {
-        string GetString<T>(T record) where T : ILogRecord;
-    }
-}
